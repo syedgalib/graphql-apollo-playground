dd5a9df2175ad8c2ed26b84f6d7776fc1e4b6453
Syed Galib Ahmed <syedgalibahmed@gmail.com>
2023-06-17 21:33:14 +0600

    improved

--- server.js
+++ server.js
@@ -13,10 +13,7 @@ const resolvers = {
     Category,
 };
 
-const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-});
+const server = new ApolloServer({ typeDefs, resolvers });
 
 const { url } = await startStandaloneServer( server, {
     listen: { port: 4000 },
